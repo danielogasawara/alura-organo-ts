3ca56e7e283b666d9877cfbae998ba5a6bdbeafc
Daniel <danielhiro13@gmail.com>
2023-01-29 14:29:05 -0300

    Adicionado o campo de data no formulário.

--- src/componentes/CampoTexto/index.tsx
+++ src/componentes/CampoTexto/index.tsx
@@ -7,9 +7,10 @@ interface CampoTextoProps {
     label: string
     valor: string
     obrigatorio?: boolean
+    tipo?: 'text' | 'password' | 'date' | 'email' | 'number'
 }
 
-const CampoTexto = ({ aoAlterado, label, placeholder, valor, obrigatorio = false }: CampoTextoProps) => {
+const CampoTexto = ({ aoAlterado, label, placeholder, valor, obrigatorio = false, tipo = 'text' }: CampoTextoProps) => {
 
     const placeholderModificada = `${placeholder}...` 
 
@@ -25,7 +26,7 @@ const CampoTexto = ({ aoAlterado, label, placeholder, valor, obrigatorio = false
             <input value={valor}
             onChange={aoDigitado}
             required={obrigatorio}
-            placeholder={placeholderModificada}/>
+            placeholder={placeholderModificada} type={tipo}/>
         </div>
     )
 }
